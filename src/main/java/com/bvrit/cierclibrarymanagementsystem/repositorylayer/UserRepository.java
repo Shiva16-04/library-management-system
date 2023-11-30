39175b5f1b67d0713d7127195f4612349a2b7da6
Shiva16-04 <shivakumaryedla1999@gmail.com>
2023-11-30 12:17:03 +0530

    corrected User Service and Repository

--- src/main/java/com/bvrit/cierclibrarymanagementsystem/repositorylayer/UserRepository.java
+++ src/main/java/com/bvrit/cierclibrarymanagementsystem/repositorylayer/UserRepository.java
@@ -3,5 +3,5 @@ package com.bvrit.cierclibrarymanagementsystem.repositorylayer;
 import com.bvrit.cierclibrarymanagementsystem.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<Integer, User> {
+public interface UserRepository extends JpaRepository<User,Integer> {
 }
